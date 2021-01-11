426d1a0da6aaab34616b1eab086d6d92be3a3f6e
Naveen Thomas Varghese <naveen.panickal@gmail.com>
2021-01-11 23:16:10 +0530

    feat: complete the design of movie index page for full-hd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { MaterialModule } from './material/material.module';
-import { ReactiveFormsModule } from '@angular/forms';
 import { MovieIndexComponent } from './movie-index/movie-index.component';
 
 @NgModule({
